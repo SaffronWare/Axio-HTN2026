d6fbbc39659f21530c9f0c1593a19dd80bc00cdf
SaffronWare <aryanshadzi@gmail.com>
2026-09-19 16:08:54 -0400

    added node editor types

---
+++ Axio/NodeEditor.h
@@ -0,0 +1,50 @@
+#include <imgui.h>
+#include <imgui_node_editor.h>
+#include <string>
+#include <vector>
+
+namespace ed = ax::NodeEditor;
+
+enum class PinType
+{
+	Input,
+	Output
+};
+
+enum class PinData
+{
+	Float,
+	Vec4,
+	Vec3,
+	Vec2
+};
+
+struct Node;
+
+struct Pin
+{
+	ed::PinId uniqueId;
+	Node* node = nullptr;
+
+	std::string name;
+	PinType type;
+	PinData data;
+
+};
+
+struct Node
+{
+	ed::NodeId uniqueId;
+	std::string name;
+
+	std::vector<Pin> inputs;
+	std::vector<Pin> outputs;
+};
+
+struct Link
+{
+	ed::LinkId uniqueId;
+	
+	Pin inputPin;
+	Pin outputPin;
+};

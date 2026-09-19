0d48018f16d31301343f6652e6e040573c9bc977
SaffronWare <aryanshadzi@gmail.com>
2026-09-19 13:17:15 -0400

    working window

--- Axio/Main.cpp
+++ Axio/Main.cpp
@@ -1,8 +1,36 @@
 ﻿#include <glad/glad.h>
 #include <glfw3.h>
 
+#include <stdexcept>
+
 
 int main()
 {
-	return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Axio", NULL, NULL);
+	if (!window)
+	{
+		throw std::runtime_error("Failed to create window!\n");
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("Failed to intialize glad!\n");
+		return -1;
+	}
+
+	glViewport(0, 0, 640, 480);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
